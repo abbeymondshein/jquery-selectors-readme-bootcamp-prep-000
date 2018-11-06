d02f5d7764f6473cde381349e75623e6bd8728fd
Abbey Mondshein <abbey.sync@gmail.com>
2018-11-06 01:45:57 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,5 +1,10 @@
 // declare your functions here...
 function paragraphSelector(){
-  let paragraphs = $('p')
+  let paragraphs = $('p');
   return paragraphs;
+}
+
+function lastImageSelector(){
+  let lastImg = $('img:last');
+  return lastImg;
 }
